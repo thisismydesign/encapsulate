b2bbc9229ea8186f967e7003bef7b061057dd656
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-06-06 03:05:39 +0200

    Remove redundant method definitions; Fix unknown variable name

--- lib/encapsulate/nested_method_chain.rb
+++ lib/encapsulate/nested_method_chain.rb
@@ -2,24 +2,16 @@ module Encapsulate
 
   class NestedMethodChain
 
-    def self.run(callback:, with:, params:)
-      run(callback, with, params)
-    end
-
-    def self.run(callback, with, params)
-      create(callback, with, params).call(params)
+    def self.run(callback:, with:, params:nil)
+      create(callback: callback, with: with).call(params)
     end
 
     def self.create(callback:, with:)
-      create(callback, with)
-    end
-
-    def self.create(callback, with)
       lambdas = []
       lambdas[0] = callback
 
-      runners.each_with_index do |runner, index|
-        lambdas[index + 1] = lambda { |params| runner.call(callback: lambdas[index], params: params) }
+      with.each_with_index do |encapsulator, index|
+        lambdas[index + 1] = lambda { |params| encapsulator.call(callback: lambdas[index], params: params) }
       end
 
       lambdas.last
